99c4f0ff7242588e6f09f19b43aa7135669d4e26
ukamal <flkamal2016@gmail.com>
2019-08-04 20:03:16 +0600

    This is Laravel user-community website completed

---
+++ resources/views/answers/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h3>Edit Answer</h3>
+                        <h3 class="ml-auto">
+                            <a class="btn btn-outline-secondary" href="{{route('questions.index')}}">Back To All Question</a>
+                        </h3>
+                    </div>
+                </div>
+               
+                <div class="card-body">
+ <form action="{{route('questions.answers.update',[$question->id,$answer->id])}}" method="post">
+               
+                 @csrf  
+                 @method('PATCH')
+               
+                 <div class="form-group">
+                     <label for="question-body">Update Your Answer</label>
+                     <textarea rows="10" id="question-body" class="form-control {{ $errors->has('body') ? ' is-invalid' : '' }}" name="body">
+                         
+                     {{old('body',$answer->body)}}
+                     </textarea>
+                     
+                     @if ($errors->has('body'))
+                          <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('body') }}</strong>
+                            </span>
+                     @endif
+                     
+                 </div>
+                 <div class="form-group">
+                     <button class="btn btn-outline-primary btn-lg" type="submit">Update This Answer</button>
+                 </div>
+                    </form>   
+               </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h3>All Questions</h3>
+                        <h3 class="ml-auto">
+                            <a class="btn btn-outline-secondary" href="{{route('questions.create')}}">Ask Question</a>
+                        </h3>
+                    </div>
+                </div>
+                 @include('layouts._message')
+                @foreach($questions as $question)
+                <div class="card-body">
+                   
+                    <div class="media">
+                        <div class="d-flex flex-column counters">
+                            <div class="vote">
+                            <strong>{{$question->votes_count}}</strong>
+                            {{str_plural('vote',$question->votes_count)}}
+                            </div>
+                            <div class="status {{$question->status}}">
+                            <strong>{{$question->answers_count}}</strong>
+                            {{str_plural('answer',$question->answers_count)}}
+                            </div>
+                            <div class="views">
+                            {{$question->views}}
+                            {{str_plural('views',$question->views)}}
+                            </div>
+                        </div>
+                        <div class="media-body">
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0">
+                                <a href="{{$question->url}}">
+                                    {{$question->title}}
+                                </a>
+                              </h3>
+                                <div class="ml-auto">
+                                    @can('update', $question)
+                                    <a class="btn btn-outline-primary btn-sm" href="{{route('questions.edit',$question->id)}}">
+                                        Edit
+                                    </a>
+                             
+                                    @endcan
+                                    @can('delete', $question)
+                                    <form class="form-delete" action="{{route('questions.destroy',$question->id)}}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button onclick="return confirm('Are You Sure')" class="btn btn-outline-danger btn-sm" type="submit">Delete</button>   
+                                    </form>
+                                    @endcan
+                                </div>
+                            </div>
+                            
+                                <p class="lead">
+                                    Asked By <a href="{{$question->user->url}}">
+                                        {{$question->user->name}}
+                                    </a>
+                                    <small class="text-muted">
+                                        {{$question->create_date}}
+                                    </small>
+                                </p>
+                           
+                            {{str_limit(strip_tags($question->body,250))}}
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                @endforeach
+               <!-- <div class="mx-auto">
+                {{$questions->links()}}
+                </div>-->
+               {{$questions->links()}}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
